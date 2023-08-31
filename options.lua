bb7e402e1e5306b21364d862c6798f0458041f37
Ian Montgomery <ian.montgomery@end-game.com>
2023-08-31 20:53:59 +1200

    moving files around

--- options.lua
+++ options.lua
@@ -7,6 +7,7 @@ return {
     spell = false, -- sets vim.opt.spell
     signcolumn = "auto", -- sets vim.opt.signcolumn to auto
     wrap = false, -- sets vim.opt.wrap
+    guifont = "MesloLGL Nerd Font"
   },
   g = {
     mapleader = " ", -- sets vim.g.mapleader
